c4789ba7783acc75f0cb9e8894ffaa53a2be0aad
Ryan Sutrisno <ryan.sutrisno@gmail.com>
2020-03-23 12:54:44 +0700

    finalize the game

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Game from './components/Game';
 
 function App() {
-  return (
-    <Game/>
-  );
+  return <Game />;
 }
 
 export default App;
